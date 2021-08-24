1c71b0cdb972ab652c05941a3220ceaae058a621
jei-0 <jei.ghub@gmail.com>
2021-08-24 07:10:00 -0700

    slim down keys for default project object

--- src/Components/ProjectsList.js
+++ src/Components/ProjectsList.js
@@ -3,29 +3,26 @@ import NewProjectButton from "./NewProjectButton";
 const ProjectLists = ({ data }) => {
   return (
     <div>
-      {data.map(
-        ({ projectID, projectTitle, projectDescription, projectIssues }, n) => {
-          return (
-            <div key={projectID + n}>
-              <h3>
-                {projectTitle}
-                <span> |{projectID}|</span>
-              </h3>
-              <p>{projectDescription}</p>
-              {
-                // show number of open issues if any
-                projectIssues.filter((issue) => issue.issueStatus === "open")
-                  .length && (
-                  <p>
-                    {projectIssues.length} Issue
-                    {projectIssues.length !== 1 && "s"} Open
-                  </p>
-                )
-              }
-            </div>
-          );
-        }
-      )}
+      {data.map(({ id, title, description, issues }, n) => {
+        return (
+          <div key={id + n}>
+            <h3>
+              {title}
+              <span> |{id}|</span>
+            </h3>
+            <p>{description}</p>
+            {
+              // show number of open issues if any
+              issues.filter((issue) => issue.status === "open").length && (
+                <p>
+                  {issues.length} Issue
+                  {issues.length !== 1 && "s"} Open
+                </p>
+              )
+            }
+          </div>
+        );
+      })}
       <NewProjectButton />
     </div>
   );
