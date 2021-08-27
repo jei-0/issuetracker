9404342fdf94570a32c50ed43a4d21e34fb47979
jei-0 <jei.ghub@gmail.com>
2021-08-27 03:04:16 -0700

    removed import of Data.json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import Header from "./Components/Header";
 import ProjectsList from "./Components/ProjectsList";
-import { DefaultInitialProject } from "./Containers/Data";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ProjectsList data={DefaultInitialProject} />
+      <ProjectsList />
     </div>
   );
 }
