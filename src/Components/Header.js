03c176bd83d77e9930d7863894e781c5e1ab826a
jei-0 <jei.ghub@gmail.com>
2021-08-29 18:47:06 -0700

    began to implement material-ui to Header comp

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,41 @@
+import { AppBar, Toolbar, Typography, IconButton } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import MailIcon from "@material-ui/icons/Mail";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import SettingsIcon from "@material-ui/icons/Settings";
+
 const Header = (props) => {
   return (
     <header>
-      <h1 className={props.className}>IssueTrackerJO</h1>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="open drawer">
+            <MenuIcon />
+          </IconButton>
+          <Typography noWrap variant="h6">
+            IssueTrackerJO
+          </Typography>
+          <div>
+            <IconButton color="inherit" aria-label="show mails">
+              <MailIcon />
+            </IconButton>
+            <IconButton color="inherit" aria-label="show notifications">
+              <NotificationsIcon />
+            </IconButton>
+            <IconButton color="inherit" aria-label="go to settings">
+              <SettingsIcon />
+            </IconButton>
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
     </header>
   );
 };
