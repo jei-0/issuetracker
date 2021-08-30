661b76e67b56054e6125e42c06787f4a581b8f13
jei-0 <jei.ghub@gmail.com>
2021-08-30 07:17:37 -0700

    removed normalize.css and stylesheet for now

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "normalize.css";
-import "./Styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
