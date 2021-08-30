c91c6b2986aaeb1adcc126f5c819265da963c5ca
jei-0 <jei.ghub@gmail.com>
2021-08-30 12:28:47 -0700

    changed material-ui theme colors and applied them

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import { createTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/core";
 import Header from "./Components/Header";
 import ProjectsList from "./Components/ProjectsList";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#232c2f",
+    },
+    secondary: {
+      main: "#fdcd47",
+    },
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <ProjectsList />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Header />
+        <ProjectsList />
+      </div>
+    </ThemeProvider>
   );
 }
 
