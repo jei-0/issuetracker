58f8502758d8ae3f2097fa6f98ec848b1bf98198
jei-0 <jei.ghub@gmail.com>
2021-09-03 00:10:46 -0700

    removed Containers folder

--- src/Containers/SaveLocalData.js
+++
@@ -1,7 +0,0 @@
-import { LocalStorageProjectsKey } from "./Data";
-
-const SaveLocalData = ({ data }) => {
-  localStorage.setItem(LocalStorageProjectsKey, JSON.stringify(data));
-};
-
-export default SaveLocalData;
